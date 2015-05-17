966a061fe12b33669d6fa96ab4f217577c4a617a
Ben Peterson <killthrush@hotmail.com>
2015-05-17 16:58:11 -0400

    final pass through pylint

--- testapp.py
+++ testapp.py
@@ -1,3 +1,7 @@
+"""
+Module that hosts a Flask API for Questions.
+"""
+
 from flask import Flask
 from flask import Response
 from flask import request
@@ -7,17 +11,13 @@ import testapphelper as h
 
 app = Flask(__name__)
 
-# TODO:
-# 6) clean up, linting
-# 7) deployment, readme
-# 8) save state
-# 9) attempt some tests
-# 10) test driver (angular?)
-
 # Keep questions in memory. This doesn't look thread-safe...
 repository = q.SimpleQuestionRepository()
 
-def initialize(app):
+def initialize():
+    """
+    Runs code when the test application starts.
+    """
     app.debug = True # allow stack traces for development
     repository.load_state_from_file('./question_state.csv')
 
@@ -28,71 +28,80 @@ def get_some_questions(id_numbers):
     Returns questions matching the given IDs in JSON format (the default), or in csv format.
         Allowed parameters:
             * fmt: set to 'csv' to return the questions in pipe-delimited format
-            * start: set to the ordinal number of the first question to return.  Must appear with num.
+            * start: set to the ordinal number of the first question to return.
+                Must appear with num.
             * num: set to the total number of questions to return.  Must appear with start.
-            * qf: set to a string; if the question text contains this string, it will be returned.  Conjunctive with 'af' and 'df' filters.
-            * af: set to a string; if the answer text contains this string, it will be returned.  Conjunctive with 'qf' and 'df' filters.
-            * df: set to a string; if the text of any of the distractors contains this string, it will be returned.  Conjunctive with 'af' and 'qf' filters.
-            * sk: set to one of the following to set the sorting key for questions: 
+            * qf: set to a string; if the question text contains this string, it will be returned.
+                Conjunctive with 'af' and 'df' filters.
+            * af: set to a string; if the answer text contains this string, it will be returned.
+                Conjunctive with 'qf' and 'df' filters.
+            * df: set to a string; if the text of any of the distractors contains this string,
+                it will be returned.  Conjunctive with 'af' and 'qf' filters.
+            * sk: set to one of the following to set the sorting key for questions:
                 * 'i': sort by id_number (the default)
                 * 'q': sort by question text
                 * 'a': sort by answer text
                 * 'd': sort by the number of distractors
-            * sd: set to one of the following to set the sorting direction for questions: 
+            * sd: set to one of the following to set the sorting direction for questions:
                 * 'a': sort ascending (the default)
-                * 'd': sort descending              
+                * 'd': sort descending
         Returns status 200 along with the formatted question data on success.
-        Returns status 400 if the ID list is malformed or if pagination arguments (start, num) are supplied but are invalid.
+        Returns status 400 if the ID list is malformed or if pagination arguments (start, num)
+            are supplied but are invalid.
         Returns status 500 on application error.
-    """    
-    if not h.is_valid_id_list(id_numbers) or (h.has_pagination_args(request) and not h.pagination_args_are_valid(request)):
+    """
+    invalid_pagination = (h.has_pagination_args(request) and not h.pagination_args_are_valid(request))
+    if not h.is_valid_id_list(id_numbers) or invalid_pagination:
         abort(400)
-    
-    questions = invoke_get_with_filters(repository, request, id_numbers)
-    return h.format_question_response(questions, request)
 
- 
+    questions = invoke_get_with_filters(repository, request.args, id_numbers)
+    return h.format_question_response(questions, request.args)
+
+
 @app.route('/questions/', methods=['GET'])
 def get_all_questions():
     """
     Returns all available questions in JSON format (the default), or in csv format.
         Allowed parameters:
             * fmt: set to 'csv' to return the questions in pipe-delimited format
-            * start: set to the ordinal number of the first question to return.  Must appear with num.
+            * start: set to the ordinal number of the first question to return.
+                Must appear with num.
             * num: set to the total number of questions to return.  Must appear with start.
-            * qf: set to a string; if the question text contains this string, it will be returned.  Conjunctive with 'af' and 'df' filters.
-            * af: set to a string; if the answer text contains this string, it will be returned.  Conjunctive with 'qf' and 'df' filters.
-            * df: set to a string; if the text of any of the distractors contains this string, it will be returned.  Conjunctive with 'af' and 'qf' filters.
-            * sk: set to one of the following to set the sorting key for questions: 
+            * qf: set to a string; if the question text contains this string, it will be returned.
+                Conjunctive with 'af' and 'df' filters.
+            * af: set to a string; if the answer text contains this string, it will be returned.
+                Conjunctive with 'qf' and 'df' filters.
+            * df: set to a string; if the text of any of the distractors contains this string,
+                it will be returned.  Conjunctive with 'af' and 'qf' filters.
+            * sk: set to one of the following to set the sorting key for questions:
                 * 'i': sort by id_number (the default)
                 * 'q': sort by question text
                 * 'a': sort by answer text
                 * 'd': sort by the number of distractors
-            * sd: set to one of the following to set the sorting direction for questions: 
+            * sd: set to one of the following to set the sorting direction for questions:
                 * 'a': sort ascending (the default)
-                * 'd': sort descending             
+                * 'd': sort descending
         Returns status 200 along with the formatted question data on success.
         Returns status 400 if pagination arguments (start, num) are supplied but are invalid.
         Returns status 500 on application error.
     """
-    if (h.has_pagination_args(request) and not h.pagination_args_are_valid(request)):
+    if h.has_pagination_args(request) and not h.pagination_args_are_valid(request):
         abort(400)
 
-    questions = invoke_get_with_filters(repository, request)
-    return h.format_question_response(questions, request)
+    questions = invoke_get_with_filters(repository, request.args)
+    return h.format_question_response(questions, request.args)
 
 
 @app.route('/questions/', methods=['POST'])
 def create_questions():
     """
-    Allows creation of one or more new questions, given a JSON payload.  Returns the new questions (with IDs) on success.
+    Allows creation of one or more new questions, given a JSON payload.
+    Returns the new questions (with IDs) on success.
         Returns status 201 along with the JSON for the new questions on success.
         Returns status 400 if the JSON payload is malformed.
         Returns status 500 on application error.
     """
     question = q.Question()
-    if not h.is_valid_int(id_number):
-        abort(400)
     if not question.try_set_from_json(request.json):
         abort(400)
     new_question = repository.create(question)
@@ -102,10 +111,12 @@ def create_questions():
 @app.route('/questions/<id_number>/', methods=['PUT'])
 def edit_question(id_number):
     """
-    Changes the data for a question given an ID and a JSON payload.  Returns the edited question on success.
+    Changes the data for a question given an ID and a JSON payload.  Returns the edited
+    question on success.
         Returns status 200 along with the JSON for the updated question on success.
         Returns status 404 if the requested question does not exist.
-        Returns status 400 if the supplied ID was not a number, the JSON payload is malformed, or the ID in the payload does not match the URL.
+        Returns status 400 if the supplied ID was not a number, the JSON payload is malformed,
+            or the ID in the payload does not match the URL.
         Returns status 500 on application error.
     """
     if not h.is_valid_int(id_number):
@@ -131,7 +142,7 @@ def remove_question(id_number):
         Returns status 500 on application error.
     """
     if not h.is_valid_int(id_number):
-        abort(400)    
+        abort(400)
     question = repository.get_by_id(id_number)
     if question == None:
         abort(404)
@@ -139,7 +150,7 @@ def remove_question(id_number):
     return '', 204
 
 
-def invoke_get_with_filters(repository, request, id_numbers = None):
+def invoke_get_with_filters(repo, args, id_numbers=None):
     """
     Given a question repository and a validated request context,
     loads questions given various parameters.
@@ -150,38 +161,38 @@ def invoke_get_with_filters(repository, request, id_numbers = None):
     id_list = None
     if isinstance(id_numbers, str):
         id_list = id_numbers.split(',')
-    
+
     # Pagination filters
     start = None
     num = None
     if h.has_pagination_args(request):
-        start = int(request.args.get('start'))
-        num = int(request.args.get('num'))
+        start = int(args.get('start'))
+        num = int(args.get('num'))
 
     # Question value filters
-    questionFilter = request.args.get('qf')
-    answerFilter = request.args.get('af')
-    distractorFilter = request.args.get('df')
+    question_filter = args.get('qf')
+    answer_filter = args.get('af')
+    distractor_filter = args.get('df')
 
     # Sorting
-    sortKey = request.args.get('sk')
-    if not isinstance(sortKey, str):
-        sortKey = 'i'
-    sort_direction = request.args.get('sd')
+    sort_key = args.get('sk')
+    if not isinstance(sort_key, str):
+        sort_key = 'i'
+    sort_direction = args.get('sd')
     if not isinstance(sort_direction, str):
         sort_direction = 'a'
 
-    questions = repository.get_with_filters(id_list = id_list, 
-                                            start_record = start, 
-                                            num_records = num,
-                                            question_filter = questionFilter,
-                                            answer_filter = answerFilter,
-                                            distractor_filter = distractorFilter,
-                                            sort_key = sortKey,
-                                            sort_descending = (sort_direction.lower() == 'd'))
+    questions = repo.get_with_filters(id_list=id_list,
+                                      start_record=start,
+                                      num_records=num,
+                                      question_filter=question_filter,
+                                      answer_filter=answer_filter,
+                                      distractor_filter=distractor_filter,
+                                      sort_key=sort_key,
+                                      sort_descending=(sort_direction.lower() == 'd'))
     return questions
 
 
 if __name__ == '__main__':
-    initialize(app)
+    initialize()
     app.run()
--- testapphelper.py
+++ testapphelper.py
@@ -1,35 +1,39 @@
-from flask import request
+"""
+Module that contains some helpful shortcut functions for the test web app.
+Helps keep the routes nice and readable.
+"""
+
 from flask import Response
 
-def format_question_response(questions, request):
+def format_question_response(questions, query_string):
     """
     Quick helper to check for formatting args and return question data in the correct format
     """
-    format = request.args.get('fmt')
-    if isinstance(format, str) and format.lower() == 'csv':
+    format_string = query_string.get('fmt')
+    if isinstance(format_string, str) and format_string.lower() == 'csv':
         csv_string = questions.to_csv()
         return Response(csv_string, mimetype='text/csv')
     json_string = questions.to_json()
     return Response(json_string, mimetype='application/json')
 
 
-def has_pagination_args(request):
+def has_pagination_args(query_string):
     """
     Quick helper to check for pagination args
     """
-    start = request.args.get('start')
-    num = request.args.get('num')
+    start = query_string.get('start')
+    num = query_string.get('num')
     return isinstance(start, str) and isinstance(num, str)
 
 
-def pagination_args_are_valid(request):
+def pagination_args_are_valid(query_string):
     """
     Quick helper to check that pagination args are valid
     """
-    if not has_pagination_args(request):
+    if not has_pagination_args(query_string):
         return False
-    start = request.args.get('start')
-    num = request.args.get('num')
+    start = query_string.get('start')
+    num = query_string.get('num')
     if not is_valid_int(start) or not is_valid_int(num):
         return False
     if int(start) < 1 or int(num) < 1:
@@ -37,33 +41,33 @@ def pagination_args_are_valid(request):
     return True
 
 
-def is_valid_id_list(idList):
+def is_valid_id_list(id_list):
     """
     Safely checks that an ID list contains a valid comma-separated list of integers
     """
-    if idList == None:
+    if id_list == None:
         return False
-    if not isinstance(idList, str):
+    if not isinstance(id_list, str):
         return False
     # strip all whitespace; if there is any, input isn't valid
-    tempIdList = ''.join(idList.split())
-    if len(idList) != len(tempIdList):
+    temp_id_list = ''.join(id_list.split())
+    if len(id_list) != len(temp_id_list):
         return False
-    ids = tempIdList.split(',')
-    for id in ids:
-        if not is_valid_int(id):
+    id_strings = temp_id_list.split(',')
+    for id_string in id_strings:
+        if not is_valid_int(id_string):
             return False
     return True
 
 
-def is_valid_int(stringToCheck):
+def is_valid_int(string_to_check):
     """
     Safely checks that a string value is an integer
     """
-    if stringToCheck == None:
+    if string_to_check == None:
         return False
     try:
-        int(stringToCheck)
+        int(string_to_check)
         return True
     except ValueError:
         return False
